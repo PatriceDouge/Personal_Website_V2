cb8c9ffdd656d00724a6966d0cab10606debbf2e
PatriceDouge <patricedouge@gmail.com>
2019-10-29 20:03:36 -0400

    added routing

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,23 +4,26 @@ import '../styles/Blog.css';
 
 class Blog extends Component {
   render() {
-    <div className="container">
+    return (
+      <div className="container">
 
-      <div className="title">
-        <hr className="hr-text" data-content="Patrice Dougé"/>
-      </div>
+        <div className="title">
+          <hr className="hr-text" data-content="Patrice Dougé"/>
+        </div>
 
-      <div className="content">
+        <div className="content">
 
-        <div className="blog1">
-        <h4>21 June 2018</h4>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <div className="blog1">
+          <h4>21 June 2018</h4>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+
         </div>
 
       </div>
+    )
 
-    </div>
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+
 import App from './components/App';
+import About from './components/About';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Work from './components/Work';
+
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/about" component={About} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/footer" component={Footer} />
+      <Route path="/home" component={Home} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/work" component={Work} />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'))
