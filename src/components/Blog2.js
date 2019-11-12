e29aa281822a3ae7a074731c986fcace1e632588
PatriceDouge <patricedouge@gmail.com>
2019-11-12 00:29:59 -0500

    adding to blog 2

--- src/components/Blog2.js
+++ src/components/Blog2.js
@@ -1,33 +1,192 @@
-import React, { Component } from 'react';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import React, { Component } from "react";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
-import '../styles/App.css';
-import '../styles/Blog2.css';
+import "../styles/App.css";
+import "../styles/Blog2.css";
 
 class Blog2 extends Component {
   render() {
     return (
       <div className="blog2">
-
         <div className="logo">
-          <Link to="/"><img src={require('../assets/images/logo.jpg')} alt="logo"/></Link>
+          <Link to="/">
+            <img src={require("../assets/images/logo.jpg")} alt="logo" />
+          </Link>
         </div>
 
         <h1 className="bold">Coding Interview Crash Course: The Must-Know</h1>
 
         <p className="date">1 November 2019</p>
 
-        <p>In progress...</p>
+        <p className="bold">Table of Contents:</p>
 
-        <p className="bold">Introduction</p>
+        <ol>
+          <li>
+            <HashLink smooth to="#intro">
+              Intro
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#bigo">
+              Big O
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#array_strings">
+              Arrays & Strings
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#linkedlists">
+              Linked Lists
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#intro">
+              Stacks & Queues
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#intro">
+              Trees and Graphs
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#intro">
+              Binary(Bit Manipulation)
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#intro">
+              Math & Logic
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#intro">
+              Recursion
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#intro">
+              Dynamic Programming
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#intro">
+              Sorting & Searching
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#intro">
+              System Design
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#intro">
+              Testing
+            </HashLink>
+          </li>
+          <li>
+            <HashLink smooth to="#intro">
+              Useful Resources
+            </HashLink>
+          </li>
+        </ol>
 
-        <p className="code-box">
-          
+        <p className="bold" id="intro">
+          Introduction
         </p>
 
-      </div>
-    )
+        <p></p>
 
+        <p className="bold" id="intro">
+          Big O
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          Arrays & Strings
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          Linked Lists
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          Stacks & Queues
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          Trees and Graphs
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          Binary Bit(Bit Manipulation)
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          Math & Logic
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          Recursion
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          Dynamic Programming
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          Sorting & Searching
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          System Design
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          System Design
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          Testing
+        </p>
+
+        <p></p>
+
+        <p className="bold" id="intro">
+          Useful Resources
+        </p>
+
+        <p></p>
+
+        <p className="code-box"></p>
+      </div>
+    );
   }
 }
 
