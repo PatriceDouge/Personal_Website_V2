61eb8a9d3c107fc27d61379c5ab3c436b9549b10
PatriceDouge <patricedouge@gmail.com>
2019-11-12 23:09:06 -0500

    fixed synthax error on projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,93 +1,112 @@
-import React, { Component } from 'react';
-import '../styles/Projects.css';
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/Projects.css";
+import "../styles/App.css";
 
 class Projects extends Component {
   render() {
     return (
       <div className="container">
         <h1>Projects</h1>
-        <h2>Want to see more? Visit my <a href="https://github.com/PatriceDouge">github.</a></h2>
+        <h2>
+          Want to see more? Visit my{" "}
+          <a href="https://github.com/PatriceDouge">github.</a>
+        </h2>
         <div className="row projects">
-
           <div className="col-sm project-box">
+            <img
+              src={require("../assets/images/github-repo-icon.jpg")}
+              alt="Repo Icon"
+            />
 
-            <img src={require('../assets/images/github-repo-icon.jpg')} alt="Repo Icon"/>
+            <a href="https://github.com/PatriceDouge/Buzz-Social-Media-App">
+              Buzz-Social-Media-App
+            </a>
 
-            <a href="https://github.com/PatriceDouge/Buzz-Social-Media-App">Buzz-Social-Media-App</a>
+            <br />
 
-            <br/>
-
-            <p>A social media app built using Angular 7, MongoDB, Express and Node. My role was handling setting up the backend, user authentication, user posting and well as styling.
+            <p>
+              A social media app built using Angular 7, MongoDB, Express and
+              Node. My role was handling setting up the backend, user
+              authentication, user posting and well as styling.
             </p>
 
             <div className="lang">
               <span className="dot-yellow"></span>
               <p className="language">JavaScript</p>
             </div>
-
           </div>
 
           <div className="col-sm project-box padding">
-
-            <img src={require('../assets/images/github-repo-icon.jpg')} alt="Repo Icon"/>
+            <img
+              src={require("../assets/images/github-repo-icon.jpg")}
+              alt="Repo Icon"
+            />
 
             <a href="https://github.com/PatriceDouge/UREats">UREats</a>
 
-            <br/>
+            <br />
 
-            <p>A nutrution and health planning solution for students at the University of Rochester. Built using React, MongoDB, Node, and Express. My role was setting up user authentication and handling the front-end and backend of the community section.</p>
+            <p>
+              A nutrution and health planning solution for students at the
+              University of Rochester. Built using React, MongoDB, Node, and
+              Express. My role was setting up user authentication and handling
+              the front-end and backend of the community section.
+            </p>
 
             <div className="lang">
-              <span class="dot-yellow"></span>
+              <span className="dot-yellow"></span>
               <p className="language">JavaScript</p>
             </div>
-
           </div>
-
         </div>
 
-         <div className="row projects">
-
+        <div className="row projects">
           <div className="col-sm project-box">
-
-            <img src={require('../assets/images/github-repo-icon.jpg')} alt="Repo Icon"/>
+            <img
+              src={require("../assets/images/github-repo-icon.jpg")}
+              alt="Repo Icon"
+            />
 
             <a href="https://github.com/PatriceDouge/Buzz-IOS">Buzz-IOS</a>
 
-            <br/>
+            <br />
 
-            <p>An IOS social media app built using Swift 4 and Google Firebase.</p>
+            <p>
+              An IOS social media app built using Swift 4 and Google Firebase.
+            </p>
 
             <div className="lang">
               <span class="dot-orange"></span>
               <p className="language">Swift</p>
             </div>
-
           </div>
 
           <div className="col-sm project-box padding">
+            <img
+              src={require("../assets/images/github-repo-icon.jpg")}
+              alt="Repo Icon"
+            />
 
-            <img src={require('../assets/images/github-repo-icon.jpg')} alt="Repo Icon"/>
+            <a href="https://github.com/PatriceDouge/Shortest-Path">
+              Shortest Path
+            </a>
 
-            <a href="https://github.com/PatriceDouge/Shortest-Path">Shortest Path</a>
+            <br />
 
-            <br/>
-
-            <p>My program reads in data with intersections and roads of a map and makes a map and calculates the shortest path between two intersections and the minimum weight spanning tree of the graph
-</p>
+            <p>
+              My program reads in data with intersections and roads of a map and
+              makes a map and calculates the shortest path between two
+              intersections and the minimum weight spanning tree of the graph
+            </p>
 
             <div className="lang">
               <span class="dot-brown"></span>
               <p className="language">Java</p>
             </div>
-
           </div>
-
-          </div>
-
+        </div>
       </div>
-    )
+    );
   }
 }
 
